9f751bdf86497562a8100d6353012f76a04beb2e
wait_fang <wait_fang@outlook.com>
2019-05-09 11:20:51 +0800

    Signed-off-by: wait_fang <wait_fang@outlook.com>

--- app.ts
+++ app.ts
@@ -3,35 +3,28 @@
 //创建时间：2019/04/27
 
 import express, { Request, Response, NextFunction } from 'express';   
-import {fsreadJsonFile,fsreadFileSync}  from  './routes/function/fun_resource';//引入配置档读取功能
-import {ExpressDecrorators} from './routes/class/ExpressDecrorators';
-import {UserController} from './routes/controllers/UserController';
 import bodyParser from 'body-parser';
 import log4js from 'log4js' //log4js 日志管理 
-import logger from 'morgan'; //nodejs 日志管理
+import {fsreadJsonFile,fsreadFileSync}  from  './routes/function/fun_resource';//引入配置档读取功能
+import {ExpressDecrorators} from './routes/class/ExpressDecrorators';
+import {UserController} from './routes/controllers/UserController'; 
 import { handler } from './routes/class/handler';
 
 // fsreadJsonFile();//异步读取json配置
 let mResourceJson  = fsreadFileSync();//同步读取
-//监听ip和端口
-let listenIP = mResourceJson.listenIP; // "localhost";
+
+let listenIP = mResourceJson.listenIP; // //监听ip和端口 "localhost";
 let listenPort=mResourceJson.listenPort;//8088; 
 let log4jsConfigure = "public\\resource\\log4js.json";//log4js 配置文件
+log4js.configure(log4jsConfigure)
 
 let app = express();  
 // app.use(bodyParser);
-// app.use(bodyParser.urlencoded({ extended: true }));
-log4js.configure(log4jsConfigure)
+// app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(log4js.connectLogger(log4js.getLogger("default"), { level: 'auto' }));
-app.use(logger("dev"));     //日志打印到控制台 
 
-// ExpressDecrorators.app = app;
+ExpressDecrorators.app = app;
 
-// UserController; //引用路由页
-
-app.get("*/xm/Chkuserinfo",(req:Request,res:Response,next :NextFunction)=>{
-    
-    res.json({message:"测试用！"})
-});
+UserController; //引用路由页
 
 app.listen(listenPort,listenIP); 
