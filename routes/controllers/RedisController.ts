48c8fb1515b86230f1ddbb2cf7d9a561689f76cf
wait_fang <wait_fang@outlook.com>
2019-05-22 09:47:12 +0800

    add redis

---
+++ routes/controllers/RedisController.ts
@@ -0,0 +1,12 @@
+import {ExpressDecrorators} from "../class/ExpressDecrorators";
+import express from 'express';
+import redis from 'redis';
+
+/**
+ * 功能说明：用来处理消息队列
+ */
+export class RedisController{
+
+
+
+}
